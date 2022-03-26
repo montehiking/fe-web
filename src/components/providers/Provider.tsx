f8077efa9a9ac073f542abbc0a4ed9e605a0df92
Kirill Neruchev <kirill@neruchev.com>
2022-03-26 10:03:45 +0100

    feat(app): use theming

--- src/components/providers/Provider.tsx
+++ src/components/providers/Provider.tsx
@@ -6,6 +6,7 @@ import { IntlProvider } from 'src/i18n/IntlProvider';
 import 'antd/dist/antd.min.css';
 
 import 'src/styles/global.css';
+import 'src/styles/theme.css';
 
 export const Provider: React.FC = ({ children }) => (
   <IntlProvider>
