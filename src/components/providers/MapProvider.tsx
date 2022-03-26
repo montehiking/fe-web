0e248e5ebf9b7db99041adfe21d1f409e8955a24
Kirill Neruchev <kirill@neruchev.com>
2022-03-26 10:36:20 +0100

    feat(app): fix tests crashes

--- src/components/providers/MapProvider.tsx
+++ src/components/providers/MapProvider.tsx
@@ -4,5 +4,10 @@ import React from 'react';
 import { GOOGLE_MAPS_API_KEY } from 'src/constants/env';
 
 export const MapProvider: React.FC = ({ children }) => (
-  <Wrapper apiKey={GOOGLE_MAPS_API_KEY}>{children}</Wrapper>
+  <Wrapper
+    apiKey={GOOGLE_MAPS_API_KEY}
+    render={() => <div data-testid="page" />}
+  >
+    {children}
+  </Wrapper>
 );
