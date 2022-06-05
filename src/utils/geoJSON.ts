0828eb9e1197ba2f78ee414d7528cc668d96b1d1
Kirill Neruchev <kirill@neruchev.com>
2022-06-05 14:51:36 +0200

    feat(map): remove temp code

--- src/utils/geoJSON.ts
+++ src/utils/geoJSON.ts
@@ -53,12 +53,3 @@ export const getData = async (isEditor: boolean): Promise<MapData> => {
 
   return { points, routes };
 };
-
-// TODO: temp code
-(async () => {
-  try {
-    await fetch('https://data.montehiking.com/points/bridge.geojson');
-  } catch (e) {
-    console.error(e);
-  }
-})();
